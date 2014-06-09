5b7cb5b8a2ba667ea169eccc1039afb93750280e
Aaron France <aaron.l.france@gmail.com>
2014-06-09 11:05:03 +0200

    Remove comment.

--- src/reploy_server/core.clj
+++ src/reploy_server/core.clj
@@ -21,7 +21,6 @@
       wrap-stacktrace-log))
 
 (defn- start-server []
-  ;; TODO: Put the port into a configuration file.
   (let [options {:port 45045}]
     (jetty/run-jetty wrap-middleware options)))
 
