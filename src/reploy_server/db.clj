6e5d44cf2f47e5a1b6b91f7a95d6824c393f3551
Aaron France <aaron.l.france@gmail.com>
2014-06-09 11:05:28 +0200

    Remove unused import.

--- src/reploy_server/db.clj
+++ src/reploy_server/db.clj
@@ -1,6 +1,5 @@
 (ns reploy-server.db
-  (:use [korma db core])
-  (:import [org.mindrot.jbcrypt BCrypt]))
+  (:use [korma db core]))
 
 (defdb main-database (postgres
                       {:db   "reploy"
