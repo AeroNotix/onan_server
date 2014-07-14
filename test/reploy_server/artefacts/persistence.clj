f8a630d7edaf70b4f029aa91312767a60744fba9
Aaron France <aaron.l.france@gmail.com>
2014-07-14 16:30:43 +0200

    Add test directory

---
+++ test/reploy_server/artefacts/persistence.clj
@@ -0,0 +1,13 @@
+(ns reploy-server.artefacts.persistence
+  (:require [reploy-server.artefacts.persistence :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest simple-artefact
+  (is false))
+
+(deftest conflicting-artefact
+  (is false))
+
+(deftest recursive-dependency
+  (is false))
