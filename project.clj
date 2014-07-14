8a62bef6bb732414c4c41752df5a9f579bb0a784
Aaron France <aaron.l.france@gmail.com>
2014-07-14 16:30:30 +0200

    Add components

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot onan-server.core
-  :dependencies [[compojure "1.1.6"]
+  :dependencies [[com.stuartsierra/component "0.2.1"]
+                 [compojure "1.1.6"]
                  [korma "0.3.0-RC5"]
                  [org.clojure/clojure "1.5.1"]
-                 [ring "1.2.1"]
+                 [org.clojure/data.json "0.2.4"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
-                 [org.clojure/data.json "0.2.4"]
+                 [ring "1.2.1"]
                  [ring/ring-json "0.2.0"]])
