8859bea423a9c43a2222d1bd29d38480a9b07f3e
Aaron France <aaron.l.france@gmail.com>
2014-07-14 17:06:02 +0200

    Widen the char columns

--- resources/db.sql
+++ resources/db.sql
@@ -3,17 +3,17 @@ CREATE EXTENSION "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
        uuid     uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
 
-       username varchar(32) NOT NULL,
-       email    varchar(60) NOT NULL,
-       password varchar(60) NOT NULL,
-       salt     varchar(29) NOT NULL
+       username varchar(64) NOT NULL,
+       email    varchar(64) NOT NULL,
+       password varchar(64) NOT NULL,
+       salt     varchar(64) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS deployment (
        uuid     uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
 
-       namespace varchar(32) NOT NULL,
-       name      varchar(32) NOT NULL,
+       namespace varchar(128) NOT NULL,
+       name      varchar(128) NOT NULL,
        version   varchar(24) NOT NULL,
        payload   bytea NOT NULL
 
