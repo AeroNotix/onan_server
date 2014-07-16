d5c66a5b4e5a613e73cad03837b1ba6204ec09bd
Aaron France <aaron.l.france@gmail.com>
2014-07-16 22:44:17 +0200

    Add cloverage, sort deps

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot onan-server.core
-  :dependencies [[com.stuartsierra/component "0.2.1"]
+  :dependencies [[cloverage "1.0.4"]
                  [compojure "1.1.6"]
                  [korma "0.3.0-RC5"]
-                 [org.clojure/test.check "0.5.8"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
+                 [org.clojure/test.check "0.5.8"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [org.postgresql/postgresql "9.2-1002-jdbc4"]
                  [ring "1.2.1"]
